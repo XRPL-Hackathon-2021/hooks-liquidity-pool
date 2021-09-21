7da6bf43bf7deca121e60d511cc9de05e05bb363
lathan britz <lathan.britz@drupalconnect.com>
2021-09-21 11:44:37 -0300

    fixed

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,12 +1,12 @@
 // The entry file of your WebAssembly module.
 
-export function cback(reserved: u32): i64 {
+export function cbak(reserved: i64): i64 {
   return 0
 }
 
 @external('_g')
 declare function _g(id: i32, maxiter: i32): void
 
-export function hook(reserved: u32): i64 {
+export function hook(reserved: i64): i64 {
   return 42
 }
