27a3e40e7c7a1ed4ba773941c165062da4bc964b
jfgrissom <jfgrissom@gmail.com>
2021-09-25 19:12:03 -0700

    Updated compiler options to those recommended by @WietseWind.

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,12 +1,17 @@
 // The entry file of your WebAssembly module.
 
+import {
+  _g as GUARD,
+  trace as TRACE
+} from '../node_modules/xrpl-hooks-api-assembly-script/dist'
+
 export function cbak(reserved: i64): i64 {
   return 0
 }
 
-@external('_g')
-declare function _g(id: i32, maxiter: i32): void
-
 export function hook(reserved: i64): i64 {
+  const message = 'Test Message'
+  TRACE(message, message.length * 2, message, message.length * 2, 0)
+  GUARD(1, 1)
   return 42
 }
