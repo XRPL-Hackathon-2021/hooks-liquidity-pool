7f18294d946e9922eba764c6f6460b6e645d557e
lathan britz <lathan.britz@drupalconnect.com>
2021-09-29 00:35:34 -0300

    WIP

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,10 +1,5 @@
 // The entry file of your WebAssembly module.
 
-// declare namespace console {
-//   function log(s: string): void
-// }
-
-
 /**
  * Guard function. Each time a loop appears in your code a call to this must be the first branch instruction after the
  * beginning of the loop.
@@ -164,10 +159,262 @@ declare function etxn_reserve(count: i32): i64
 @external('env', 'etxn_generation')
 declare function etxn_generation(): void
 
+/**
+ * Emit a transaction from this hook.
+ * @param read_ptr Memory location of a buffer containing the fully formed binary transaction to emit.
+ * @param read_len The length of the transaction.
+ * @return A negative integer if the emission failed.
+ */
+ @external('env', 'emit')
+declare function emit(write_ptr: i32, write_len: i32, read_ptr: i32, read_len: i32): i64
+
+/**
+ * Retrieve the account the hook is running on.
+ * @param write_ptr A buffer of at least 20 bytes to write into.
+ * @param write_len The length of that buffer
+ * @return The number of bytes written into the buffer of a negative integer if an error occured.
+ */
+@external('env', 'hook_account')
+declare function hook_account(write_ptr: i32, write_len: i32): i64
+
+/**
+ * Retrieve the hash of the currently executing hook.
+ * @param write_ptr A buffer of at least 32 bytes to write into.
+ * @param write_len The length of that buffer
+ * @return The number of bytes written into the buffer of a negative integer if an error occured.
+ */
+@external('env', 'emhook_hashit')
+declare function hook_hash(write_ptr: i32, write_len: i32): i64
+
+/**
+ * Retrive the currently recommended minimum fee for a transaction to succeed.
+ */
+@external('env', 'fee_base')
+declare function fee_base(): void
+
+/**
+ * Retrieve the current ledger sequence number
+ */
+@external('env', 'ledger_seq')
+declare function ledger_seq(): void
+
+@external('env', 'ledger_last_hash')
+declare function ledger_last_hash(write_ptr: i32, write_len: i32): i64
+
+/**
+ * Retrieve a nonce for use in an emitted transaction (or another task). Can be called repeatedly for multiple nonces.
+ * @param write_ptr A buffer of at least 32 bytes to write into.
+ * @param write_len The length of that buffer
+ * @return The number of bytes written into the buffer of a negative integer if an error occured.
+ */
+@external('env', 'nonce')
+declare function nonce(write_ptr: i32, write_len: i32): i64
 
 
+/**
+ * Slot functions have not been implemented yet and the api for them is subject to change
+ */
+@external('env', 'slot')
+declare function slot(write_ptr: i32, write_len: i32, slot: i32): i64
+
+@external('env', 'slot_clear')
+declare function slot_clear(slot: i32): i64
+
+@external('env', 'slot_count')
+declare function slot_count(slot: i32): i64
+
+@external('env', 'slot_id')
+declare function slot_id(slot: i32): i64
+
+@external('env', 'slot_set')
+declare function slot_set(read_ptr: i32, read_len: i32, slot: i32): i64
+
+@external('env', 'slot_size')
+declare function slot_size(slot: i32): i64
+
+@external('env', 'slot_subarray')
+declare function slot_subarray(parent_slot: i32, array_id: i32, new_slot: i32): i64
+
+@external('env', 'slot_subfield')
+declare function slot_subfield(parent_slot: i32, field_id: i32, new_slot: i32): i64
+
+@external('env', 'slot_type')
+declare function slot_type(slot: i32, flags: i32): i64
+
+@external('env', 'slot_float')
+declare function slot_float(slot: i32): i64
+
+@external('env', 'trace_slot')
+declare function trace_slot(mread_ptr: i32, mread_len: i32, slot: i32): i64
+
+@external('env', 'otxn_slot')
+declare function otxn_slot(slot: i32): i64
+
+
+/**
+ * In the hook's state key-value map, set the value for the key pointed at by kread_ptr.
+ * @param read_ptr A buffer containing the data to store
+ * @param read_len The length of the data
+ * @param kread_ptr A buffer containing the key
+ * @param kread_len The length of the key
+ * @return The number of bytes stored or a negative integer if an error occured
+ */
+@external('env', 'state_set')
+declare function state_set(read_ptr: i32, read_len: i32, kread_ptr: i32, kread_len: i32): i64
+
+/**
+ * Retrieve a value from the hook's key-value map.
+ * @param write_ptr A buffer to write the state value into
+ * @param write_len The length of that buffer
+ * @param kread_ptr A buffer to read the state key from
+ * @param kread_len The length of that key
+ * @return The number of bytes written or a negative integer if an error occured.
+ */
+@external('env', 'state')
+declare function state(write_ptr: i32, write_len: i32, kread_ptr: i32, kread_len: i32): i64
+
+/**
+ * Retrieve a value from another hook's key-value map.
+ * @param write_ptr A buffer to write the state value into
+ * @param write_len The length of that buffer
+ * @param kread_ptr A buffer to read the state key from
+ * @param kread_len The length of that key
+ * @param aread_ptr A buffer containing an account-id of another account containing a hook whose state we are reading
+ * @param aread_len The length of the account-id (should always be 20).
+ * @return The number of bytes written or a negative integer if an error occured.
+ */
+@external('env', 'state_foreign')
+declare function state_foreign(write_ptr: i32, write_len: i32, kread_ptr: i32, kread_len: i32, aread_ptr: i32,  aread_len: i32): i64
+
+/**
+ * Print some output to the trace log on xrpld. Any xrpld instance set to "trace" log level will see this.
+ * @param read_ptr A buffer containing either data or text (in either utf8, or utf16le)
+ * @param read_len The byte length of the data/text to send to the trace log
+ * @param as_hex If 0 treat the read_ptr as pointing at a string of text, otherwise treat it as data and print hex
+ * @return The number of bytes output or a negative integer if an error occured.
+ */
 @external('env', 'trace')
 declare function trace(mread_ptr: string, mread_len: i32, dread_ptr: string, dread_len: i32, as_hex: i32): i64
+
+/**
+ * Print some output to the trace log on xrpld along with a decimal number. Any xrpld instance set to "trace" log
+ * level will see this.
+ * @param read_ptr A pointer to the string to output
+ * @param read_len The length of the string to output
+ * @param number Any integer you wish to display after the text
+ * @return A negative value on error
+ */
+@external('env', 'trace_num')
+declare function trace_num(read_ptr: i32, read_len: i32, number: i64): i64
+
+/**
+ * Retrieve the burden of the originating transaction (if any)
+ * @return The burden of the originating transaction
+ */
+@external('env', 'otxn_burden')
+declare function otxn_burden(): void
+
+/**
+ * Retrieve a field from the originating transaction as "full text" (The way it is displayed in JSON)
+ * @param write_ptr A buffer to write the representation into
+ * @param write_len The length of the buffer
+ * @param field_id The field code of the field being requested
+ * @return The number of bytes written to write_ptr or a negative integer if an error occured.
+ */
+@external('env', 'otxn_field_txt')
+declare function otxn_field_txt(write_ptr: i32, write_len: i32, field_id: i32): i64
+
+/**
+ * Retrieve a field from the originating transaction in its raw serialized form.
+ * @param write_ptr A buffer to output the field into
+ * @param write_len The length of the buffer.
+ * @param field_if The field code of the field being requested
+ * @return The number of bytes written to write_ptr or a negative integer if an error occured.
+ */
+@external('env', 'otxn_field')
+declare function otxn_field(write_ptr: i32, write_len: i32, field_id: i32): i64
+
+/**
+ * Retrieve the generation of the originating transaction (if any).
+ * @return the generation of the originating transaction.
+ */
+@external('env', 'otxn_generation')
+declare function otxn_generation(): void
+
+/**
+ * Retrieve the TXNID of the originating transaction.
+ * @param write_ptr A buffer at least 32 bytes long
+ * @param write_len The length of the buffer.
+ * @return The number of bytes written into the buffer or a negative integer on failure.
+ */
+@external('env', 'otxn_id')
+declare function otxn_id(write_ptr: i32, write_len: i32): i64
+
+/**
+ * Retrieve the Transaction Type (e.g. ttPayment = 0) of the originating transaction.
+ * @return The Transaction Type (tt-code)
+ */
+@external('env', 'otxn_type')
+declare function otxn_type(): i64
+
+@external('env', 'float_set')
+declare function float_set(exponent: i32, mantissa: i64): i64
+
+@external('env', 'float_multiply')
+declare function float_multiply(float1: i64, float2: i64): i64
+
+@external('env', 'float_mulratio')
+declare function float_mulratio(float1: i64, round_up: i32, numerator: i32, denominator: i32): i64
+
+@external('env', 'float_negate')
+declare function float_negate(float1: i64): i64
+
+@external('env', 'float_compare')
+declare function float_compare(float1: i64, float2: i64, mode: i32): i64
+
+@external('env', 'float_sum')
+declare function float_sum(float1: i64, float2: i64): i64
+
+@external('env', 'float_sto')
+declare function float_sto(write_ptr: i32, write_len: i32, cread_ptr: i32, cread_len: i32, iread_ptr: i32, iread_le: i32n, float1: i64, field_code: i32): i64
+
+@external('env', 'float_sto_set')
+declare function float_sto_set(read_ptr: i32, read_len: i32): i64
+
+@external('env', 'float_invert')
+declare function float_invert(float1: i64): i64
+
+@external('env', 'float_divide')
+declare function float_divide(float1: i64, float2: i64): i64
+
+@external('env', 'float_one')
+declare function float_one(): i64
+
+@external('env', 'float_exponent')
+declare function float_exponent(float1: i64): i64
+
+@external('env', 'float_exponent_set')
+declare function float_exponent_set(float1: i64, exponent: i32): i64
+
+@external('env', 'float_mantissa')
+declare function float_mantissa(float1: i64): i64
+
+@external('env', 'float_mantissa_set')
+declare function float_mantissa_set(float1: i64, mantissa: i64): i64
+
+@external('env', 'float_sign')
+declare function float_sign(float1: i64): i64
+
+@external('env', 'float_sign_set')
+declare function float_sign_set(float1: i64, negative: i32): i64
+
+@external('env', 'float_int')
+declare function float_int(float1: i64, decimal_places: i32, abs: i32): i64
+
+@external('env', 'trace_float')
+declare function trace_float(mread_ptr: i32, mread_len: i32, float1: i64): i64
+
+
 
 export function cbak(reserved: i64): i64 {
   return 0
@@ -186,3 +433,4 @@ export function hook(reserved: i64): i64 {
 
   return 0
 }
+
