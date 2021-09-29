4116e317790d9ece76b8a6a33ab92bfb275f56a9
lathan britz <lathan.britz@drupalconnect.com>
2021-09-29 00:10:33 -0300

    WIP

--- assembly/index.ts
+++ assembly/index.ts
@@ -30,7 +30,7 @@ declare function accept(read_ptr: string, read_len: i32, error_code: i64): i64
  * @param read_len The length of the string. May be 0.
  * @return Will never return, terminates the hook.
  */
- @external('env', 'rollback')
+@external('env', 'rollback')
 declare function rollback(read_ptr: i32, read_len: i32, error_code: i64): i64
 
 /**
@@ -43,6 +43,7 @@ declare function rollback(read_ptr: i32, read_len: i32, error_code: i64): i64
  * @return On success the length of the r-address will be returned indicating the bytes written to the write buffer.
  *         On failure a negative integer is returned indicating what went wrong.
  */
+@external('env', 'util_raddr')
 declare function util_raddr(write_ptr: i32, write_len: i32, read_ptr: i32, read_len: i32): i64
 
 /**
@@ -55,6 +56,7 @@ declare function util_raddr(write_ptr: i32, write_len: i32, read_ptr: i32, read_
  * @return On success 20 will be returned indicating the bytes written. On failure a negative integer is returned
  *         indicating what went wrong.
  */
+@external('env', 'util_accid')
 declare function util_accid(write_ptr: i32, write_len: i32, read_ptr: i32,  read_len: i32): i64
 
 /**
@@ -67,6 +69,7 @@ declare function util_accid(write_ptr: i32, write_len: i32, read_ptr: i32,  read
  * @param kread_len The length of the public key
  * @return True if and only if the signature was verified.
  */
+@external('env', 'util_verify')
 declare function util_verify(dread_ptr: i32, dread_len: i32, sread_ptr: i32, sread_len: i32, kread_ptr: i32, kread_len: i32): i64
 
 /**
@@ -77,6 +80,7 @@ declare function util_verify(dread_ptr: i32, dread_len: i32, sread_ptr: i32, sre
  * @param read_len  The amount of data to read from the buffer.
  * @return The number of bytes written to write_ptr or a negative integer on error.
  */
+@external('env', 'util_sha512h')
 declare function util_sha512h(write_ptr: i32, write_len: i32, read_ptr: i32,  read_len: i32): i64
 
 /**
@@ -90,6 +94,7 @@ declare function util_sha512h(write_ptr: i32, write_len: i32, read_ptr: i32,  re
  *         to read_ptr and the low-word (least significant 4 bytes) is its length. MSB is sign bit, if set (negative)
  *         return value indicates error (typically error means could not find.)
  */
+@external('env', 'sto_subfield')
 declare function sto_subfield(read_ptr: i32, read_len: i32, field_id: i32): i64
 
 
@@ -104,25 +109,65 @@ declare function sto_subfield(read_ptr: i32, read_len: i32, field_id: i32): i64
  *         to read_ptr and the low-word (least significant 4 bytes) is its length. MSB is sign bit, if set (negative)
  *         return value indicates error (typically error means could not find.)
  */
+@external('env', 'sto_subarray')
 declare function sto_subarray(read_ptr: i32,  read_len: i32, array_id: i32): i64
 
+@external('env', 'sto_validate')
 declare function sto_validate(read_ptr: i32,  read_len: i32): i64
 
+@external('env', 'sto_emplace')
 declare function sto_emplace(write_ptr: i32, write_len: i32, sread_ptr: i32, sread_len: i32, fread_ptr: i32, fread_len: i32, field_id: i32): i64
 
+@external('env', 'sto_erase')
 declare function sto_erase(write_ptr: i32,  write_len: i32, read_ptr: i32, read_len: i32, field_id: i32): i64
 
+@external('env', 'util_keylet')
 declare function util_keylet(write_ptr: i32, write_len: i32, keylet_type: i32, a: i32, b: i32, c: i32, d: i32, e: i32, f: i32): i64
 
+/**
+ * Compute burden for an emitted transaction.
+ * @return the burden a theoretically emitted transaction would have.
+ */
+@external('env', 'etxn_burden')
+declare function etxn_burden(): void
+
+/**
+ * Write a full emit_details stobject into the buffer specified.
+ * @param write_ptr A sufficiently large buffer to write into.
+ * @param write_len The length of that buffer.
+ * @return The number of bytes written or a negative integer indicating an error.
+ */
+@external('env', 'etxn_details')
+declare function etxn_details(write_ptr: i32, write_len: i32): i64
+
+/**
+ * Compute the minimum fee required to be paid by a hypothetically emitted transaction based on its size in bytes.
+ * @param The size of the emitted transaction in bytes
+ * @return The minimum fee in drops this transaction should pay to succeed
+ */
+@external('env', 'etxn_fee_base')
+declare function etxn_fee_base(tx_byte_count: i32): i64
+
+/**
+ * Inform xrpld that you will be emitting at most @count@ transactions during the course of this hook execution.
+ * @param count The number of transactions you intend to emit from this  hook.
+ * @return If a negaitve integer an error has occured
+ */
+@external('env', 'etxn_reserve')
+declare function etxn_reserve(count: i32): i64
+
+/**
+ * Compute the generation of an emitted transaction. If this hook was invoked by a transaction emitted by a previous
+ * hook then the generation counter will be 1+ the previous generation counter otherwise it will be 1.
+ * @return The generation of a hypothetically emitted transaction.
+ */
+@external('env', 'etxn_generation')
+declare function etxn_generation(): void
 
 
 
 @external('env', 'trace')
 declare function trace(mread_ptr: string, mread_len: i32, dread_ptr: string, dread_len: i32, as_hex: i32): i64
-
-@external('env', 'etxn_reserve')
-declare function etxn_reserve(count: i32): i64
-
 
 export function cbak(reserved: i64): i64 {
   return 0
